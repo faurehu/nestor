7a1494c21be43e6ffb89ef3362bfe4b71b6bbbf4
Faure Hu <hi@faure.hu>
2016-10-03 17:23:55 +0100

    Missing file

--- nestor/__init__.py
+++ nestor/__init__.py
@@ -2,6 +2,11 @@ from nestor.app import app
 from nestor.routes import routes
 from nestor.logger import logger
 
-def create_app():
+def create_app(testing=False):
+
+    app.config.update(
+        TESTING=testing
+    )
+
     app.register_blueprint(routes)
     return app
